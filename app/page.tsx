ed55a07a489794125f90760713abea89e30ed0db
Super Jump <superjump.void@gmail.com>
2025-02-07 00:49:18 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from '@/components/ui/card'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-screen flex justify-center items-center">
+      <Card className="w-[350px]">
+        <CardContent>
+
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
